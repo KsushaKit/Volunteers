cb85b2c31d1f0b6aacc5d6ae222c5d63b79b123f
KsushaKit <ksushazamaraeva@gmail.com>
2023-11-13 10:30:15 +0300

    form

--- php/admin.php
+++ php/admin.php
@@ -1,3 +1,4 @@
+<!-- Получение данных (заявок) -->
 <?php 
     include "applicationGetData.php";
 ?>
@@ -8,10 +9,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Панель администратора</title>
+    <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/admin.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Выгрузка данных -->
     <script>
+        
         $(document).ready(function() {
             $('.exportButton').click(function() {
                 window.location.href = 'exportAllApplications.php';
@@ -32,146 +35,168 @@
                 window.location.href = 'exportApplications3.php';
             });
         });
+        
     </script>
     
 </head>
 <body>
-    <h1>Панель администратора</h1>
 
-    <!-- Вкладки -->
-    <button onclick="openTab('all')">Все заявки</button>
-    <button onclick="openTab('type1')">Адресная</button>
-    <button onclick="openTab('type2')">Гуманитарная</button>
-    <button onclick="openTab('type3')">Психологическая</button>
-    <br><br>
+        <div class="panel">
+            <h1>Панель администратора</h1>
 
-   <!-- Все заявки -->
-    <div id="all" class="tab">
-        <button class="exportButton">Выгрузить в exel</button>
-        <h2>Все заявки</h2>
-        <?php
-            if ($result): 
-        ?> 
+            <!-- Вкладки -->
+            <div class="panel-buttons">
+                <button class="panel-button" onclick="openTab('all')">Все заявки</button>
+                <button class="panel-button" onclick="openTab('type1')">Адресная</button>
+                <button class="panel-button" onclick="openTab('type2')">Гуманитарная</button>
+                <button class="panel-button" onclick="openTab('type3')">Психологическая</button>
+            </div>
+        </div>
+
+    <div class="content-wrapper">
+
+        <!-- Все заявки -->
+        <div id="all" class="tab">
+            <h2>Все заявки</h2>
+            <?php
+                if ($result): 
+            ?> 
+                <div class="button-export-wrapper">
+                    <button class="exportButton">Выгрузить в exel</button>
+                </div>
             
-            <table>
-                <tr>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Номер телефона</th>
-                    <th>Тип заявки</th>
-                    <th>Комментарий</th>
-                </tr>
-                <?php foreach ($result as $row): ?>
+                <table>
                     <tr>
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['surname']; ?></td>
-                        <td><?php echo $row['phone']; ?></td>
-                        <td><?php echo $row['type_of_assistance']; ?></td>
-                        <td><?php echo $row['comment']; ?></td>
+                        <th>Имя</th>
+                        <th>Фамилия</th>
+                        <th>Номер телефона</th>
+                        <th>Тип заявки</th>
+                        <th>Комментарий</th>
                     </tr>
-                <?php endforeach; ?> 
-            </table>
+                    <?php foreach ($result as $row): ?>
+                        <tr>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['surname']; ?></td>
+                            <td><?php echo $row['phone']; ?></td>
+                            <td><?php echo $row['type_of_assistance']; ?></td>
+                            <td><?php echo $row['comment']; ?></td>
+                        </tr>
+                    <?php endforeach; ?> 
+                </table>
 
-        <?php else: ?>
-            <p>Нет данных для отображения</p>
-        <?php endif; ?>
-    </div>
- 
-    <!-- Заявки типа 1 -->
-    <div id="type1" class="tab">
-    <button class="exportButton1">Выгрузить в exel</button>
-        <h2>Адресная</h2>
-        <?php
-            if ($result): 
-        ?> 
-            
-            <table>
-                <tr>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Номер телефона</th>
-                    <th>Тип заявки</th>
-                    <th>Комментарий</th>
-                </tr>
-                <?php foreach ($resultAddress as $row): ?>
+            <?php else: ?>
+                <p>Нет данных для отображения</p>
+            <?php endif; ?>
+        </div>
+
+        <!-- Заявки типа 1 -->
+        <div id="type1" class="tab">
+
+            <h2>Адресная</h2>
+            <?php
+                if ($resultAddress): 
+            ?> 
+                <div class="button-export-wrapper">
+                    <button class="exportButton1">Выгрузить в exel</button>
+                </div>
+
+                <table>
                     <tr>
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['surname']; ?></td>
-                        <td><?php echo $row['phone']; ?></td>
-                        <td><?php echo $row['type_of_assistance']; ?></td>
-                        <td><?php echo $row['comment']; ?></td>
+                        <th>Имя</th>
+                        <th>Фамилия</th>
+                        <th>Номер телефона</th>
+                        <th>Тип заявки</th>
+                        <th>Комментарий</th>
                     </tr>
-                <?php endforeach; ?> 
-            </table>
+                    <?php foreach ($resultAddress as $row): ?>
+                        <tr>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['surname']; ?></td>
+                            <td><?php echo $row['phone']; ?></td>
+                            <td><?php echo $row['type_of_assistance']; ?></td>
+                            <td><?php echo $row['comment']; ?></td>
+                        </tr>
+                    <?php endforeach; ?> 
+                </table>
 
-        <?php else: ?>
-            <p>Нет данных для отображения</p>
-        <?php endif; ?>
-    </div>
+            <?php else: ?>
+                <p>Нет данных для отображения</p>
+            <?php endif; ?>
+        </div>
 
-    <!-- Заявки типа 2 -->
-    <div id="type2" class="tab">
-    <button class="exportButton2">Выгрузить в exel</button>
-        <h2>Гуманитарная</h2>
-        <?php
-            if ($result): 
-        ?> 
-            
-            <table>
-                <tr>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Номер телефона</th>
-                    <th>Тип заявки</th>
-                    <th>Комментарий</th>
-                </tr>
-                <?php foreach ($resultHumanitarian as $row): ?>
+        <!-- Заявки типа 2 -->
+        <div id="type2" class="tab">
+
+            <h2>Гуманитарная</h2>
+            <?php
+                if ($resultHumanitarian): 
+            ?> 
+                <div class="button-export-wrapper">
+                    <button class="exportButton2">Выгрузить в exel</button>
+                </div>
+
+                <table>
                     <tr>
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['surname']; ?></td>
-                        <td><?php echo $row['phone']; ?></td>
-                        <td><?php echo $row['type_of_assistance']; ?></td>
-                        <td><?php echo $row['comment']; ?></td>
+                        <th>Имя</th>
+                        <th>Фамилия</th>
+                        <th>Номер телефона</th>
+                        <th>Тип заявки</th>
+                        <th>Комментарий</th>
                     </tr>
-                <?php endforeach; ?> 
-            </table>
+                    <?php foreach ($resultHumanitarian as $row): ?>
+                        <tr>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['surname']; ?></td>
+                            <td><?php echo $row['phone']; ?></td>
+                            <td><?php echo $row['type_of_assistance']; ?></td>
+                            <td><?php echo $row['comment']; ?></td>
+                        </tr>
+                    <?php endforeach; ?> 
+                </table>
 
-        <?php else: ?>
-            <p>Нет данных для отображения</p>
-        <?php endif; ?>
-    </div>
+            <?php else: ?>
+                <p>Нет данных для отображения</p>
+            <?php endif; ?>
+        </div>
 
-    <!-- Заявки типа 3 -->
-    <div id="type3" class="tab">
-    <button class="exportButton3">Выгрузить в exel</button>
-        <h2>Психологическая</h2>
-        <?php
-            if ($result): 
-        ?> 
-            
-            <table>
-                <tr>
-                    <th>Имя</th>
-                    <th>Фамилия</th>
-                    <th>Номер телефона</th>
-                    <th>Тип заявки</th>
-                    <th>Комментарий</th>
-                </tr>
-                <?php foreach ($resultPsychological as $row): ?>
+        <!-- Заявки типа 3 -->
+        <div id="type3" class="tab">
+
+            <h2>Психологическая</h2>
+            <?php
+                if ($resultPsychological): 
+            ?> 
+                <div class="button-export-wrapper">
+                    <button class="exportButton3">Выгрузить в exel</button>
+                </div>
+
+                <table>
                     <tr>
-                        <td><?php echo $row['name']; ?></td>
-                        <td><?php echo $row['surname']; ?></td>
-                        <td><?php echo $row['phone']; ?></td>
-                        <td><?php echo $row['type_of_assistance']; ?></td>
-                        <td><?php echo $row['comment']; ?></td>
+                        <th>Имя</th>
+                        <th>Фамилия</th>
+                        <th>Номер телефона</th>
+                        <th>Тип заявки</th>
+                        <th>Комментарий</th>
                     </tr>
-                <?php endforeach; ?> 
-            </table>
+                    <?php foreach ($resultPsychological as $row): ?>
+                        <tr>
+                            <td><?php echo $row['name']; ?></td>
+                            <td><?php echo $row['surname']; ?></td>
+                            <td><?php echo $row['phone']; ?></td>
+                            <td><?php echo $row['type_of_assistance']; ?></td>
+                            <td><?php echo $row['comment']; ?></td>
+                        </tr>
+                    <?php endforeach; ?> 
+                </table>
 
-        <?php else: ?>
-            <p>Нет данных для отображения</p>
-        <?php endif; ?>
+            <?php else: ?>
+                <p>Нет данных для отображения</p>
+            <?php endif; ?>
+        </div>
+
+
     </div>
+
 <script src="../js/admin.js"></script>
 </body>
 
---
+++ php/form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/form.css">
+</head>
+
+<body>
+  <div class="container">
+    <div id="logbox">
+      <form id="signup" method="post" action="/signup">
+        <h1>Вход в панель администратора</h1>
+        <!-- <input name="user[name]" type="text" placeholder="What's your username?" pattern="^[\\w]{3,16}$" autofocus required class="input pass"> -->
+        <input name="password2" type="password" placeholder="Пароль" required class="input pass">
+        <input name="email" type="email" placeholder="Email" class="input pass">
+        <input type="submit" value="Войти" class="inputButton">
+        <a href="#" class="restore-pass" role="button">Восстановить пароль</a>
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
